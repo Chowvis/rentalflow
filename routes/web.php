982bb8011a38c9737d3d8b1040b7352ed0cd591c
Chowvis <chvishallongchat98@gmail.com>
2024-01-17 15:19:15 +0530

    17/01 progress to home page
    make it resposnsive

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\homepagecontroller;
+use App\Http\Controllers\registrationcontroller;
+use App\Http\Controllers\storecontroller;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[homepagecontroller::class,'home'])->name('homepage');
+Route::get('/signup',[registrationcontroller::class,'signup'])->name('signuppage');
+
+Route::post('/store',[storecontroller::class,'store'])->name('store');
+
